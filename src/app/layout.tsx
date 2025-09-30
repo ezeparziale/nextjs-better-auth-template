c7bda7b43c86c6f8d24b2ddb725c545afd389e31
Ezequiel Parziale <eparziale@gmail.com>
2025-09-30 19:49:14 -0300

    :zap: chore: add theme providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Toaster } from "@/components/ui/sonner"
 import "./globals.css"
+import Providers from "@/components/providers"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +25,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <main>{children}</main>
+        <Providers>{children}</Providers>
         <Toaster richColors />
       </body>
     </html>
