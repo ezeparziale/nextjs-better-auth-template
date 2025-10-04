4a6a277947f4ef5cc6d3b08c9d222f6b884cc8c3
Ezequiel Parziale <eparziale@gmail.com>
2025-10-03 21:00:53 -0300

    :art: chore: improve separator background

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -163,7 +163,7 @@ export default function LogInForm() {
                   </Button>
 
                   <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                    <span className="text-muted-foreground relative z-10 px-2">
+                    <span className="bg-card text-muted-foreground relative z-10 px-2">
                       Or continue with
                     </span>
                   </div>
