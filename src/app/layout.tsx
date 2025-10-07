b6a70c75dc84e2ad033e85c8969e78a6d3a30d27
Ezequiel Parziale <eparziale@gmail.com>
2025-10-06 21:19:01 -0300

    :zap: chore: move globals.css to styles folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Toaster } from "@/components/ui/sonner"
-import "./globals.css"
 import Providers from "@/components/providers"
+import "@/styles/globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
