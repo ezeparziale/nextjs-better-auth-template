0ae81a17afe0831147b4572b6cf5ba330e961ef4
Ezequiel Parziale <eparziale@gmail.com>
2025-10-22 20:36:35 -0300

    :zap: chore: add loading state to logout page

---
+++ src/app/(auth)/logout/loading.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@/components/ui/spinner"
+
+export default function LoadingLogoutPage() {
+  return (
+    <div className="flex flex-col items-center">
+      <Spinner />
+    </div>
+  )
+}
