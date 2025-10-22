6d2006370bf0f0977a88fa873f3040e5e32b52d9
Ezequiel Parziale <eparziale@gmail.com>
2025-10-22 19:31:09 -0300

    :bug: fix: add missing import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import { defineConfig, globalIgnores } from "eslint/config"
 
 const eslintConfig = defineConfig([
