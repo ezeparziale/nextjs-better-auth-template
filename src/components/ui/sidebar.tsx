02bc871fe17a4e156c81055db13299337d7dba87
Ezequiel Parziale <eparziale@gmail.com>
2025-10-25 14:55:21 -0300

    :zap: chore: upgrade ui comp

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { useIsMobile } from "@/hooks/use-mobile"
 import { Slot } from "@radix-ui/react-slot"
-import { cva, VariantProps } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
