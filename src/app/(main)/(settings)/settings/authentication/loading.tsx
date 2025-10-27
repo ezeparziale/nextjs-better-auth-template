81c913bf6985deb02d7e2d9f29044eb4f34b9be0
Ezequiel Parziale <eparziale@gmail.com>
2025-10-27 20:54:55 -0300

    :zap: chore: add page header loading page

--- src/app/(main)/(settings)/settings/authentication/loading.tsx
+++ src/app/(main)/(settings)/settings/authentication/loading.tsx
@@ -1,5 +1,16 @@
+import { PageHeader } from "@/components/page-header"
 import ProvidersListSkeleton from "./_components/providers-list-skeleton"
 
+const PAGE = {
+  title: "Authentication",
+  description: "Manage your authentication settings.",
+}
+
 export default function AuthenticationLoadingPage() {
-  return <ProvidersListSkeleton />
+  return (
+    <div className="space-y-6">
+      <PageHeader title={PAGE.title} description={PAGE.description} isSection />
+      <ProvidersListSkeleton />
+    </div>
+  )
 }
