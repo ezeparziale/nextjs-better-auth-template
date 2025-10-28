6ae1726e61e5296a953c2f49743a5cdaa9006d2e
Ezequiel Parziale <eparziale@gmail.com>
2025-10-28 19:33:57 -0300

    :zap: chore: update sidebar component with routing and admin items

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,7 @@
-import { LayoutDashboard } from "lucide-react"
+"use client"
+
+import { usePathname } from "next/navigation"
+import { Building, Key, LayoutDashboard, Shield, Users } from "lucide-react"
 import {
   Sidebar,
   SidebarContent,
@@ -14,12 +17,43 @@ import {
 const items = [
   {
     title: "Dashboard",
-    url: "#",
+    url: "/dashboard",
     icon: LayoutDashboard,
   },
 ]
 
+// TODO: check if user is admin.
+const adminItems = [
+  {
+    title: "Dashboard",
+    url: "/admin/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Users",
+    url: "/admin/users",
+    icon: Users,
+  },
+  {
+    title: "Roles",
+    url: "/admin/roles",
+    icon: Shield,
+  },
+  {
+    title: "Permissions",
+    url: "/admin/permissions",
+    icon: Key,
+  },
+  {
+    title: "Organizations",
+    url: "/admin/organizations",
+    icon: Building,
+  },
+]
+
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const pathname = usePathname()
+
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarContent>
@@ -29,7 +63,24 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Admin</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {adminItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
                     <a href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
