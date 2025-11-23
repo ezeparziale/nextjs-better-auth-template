4ba4bf0560b81a46df0bea8e671d4198ed567c79
Ezequiel Parziale <eparziale@gmail.com>
2025-11-22 22:26:27 -0300

    :fire: chore: remove unused import

--- src/app/(main)/(admin)/admin/roles/_components/delete-role-dialog.tsx
+++ src/app/(main)/(admin)/admin/roles/_components/delete-role-dialog.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { router } from "better-auth/api"
 import { Controller, useForm } from "react-hook-form"
 import { toast } from "sonner"
 import z from "zod"
