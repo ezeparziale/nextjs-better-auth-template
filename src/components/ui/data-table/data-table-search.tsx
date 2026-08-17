adf9d840c5634486c9cd0fde710acf9a65d5ab78
Ezequiel Parziale <eparziale@gmail.com>
2026-08-17 17:23:34 -0300

    :fire: chore: remove unused import

--- src/components/ui/data-table/data-table-search.tsx
+++ src/components/ui/data-table/data-table-search.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import { Search, X } from "lucide-react"
 import { useDebouncedCallback } from "use-debounce"
 import {
