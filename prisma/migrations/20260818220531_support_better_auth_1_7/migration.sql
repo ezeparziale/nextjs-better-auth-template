4a42601874b83195ca67a2a33add9eef9775e2d7
Ezequiel Parziale <eparziale@gmail.com>
2026-08-19 19:05:02 -0300

    :card_file_box: feat: add issuer column to account table and update failedVerificationCount in two_factor model

---
+++ prisma/migrations/20260818220531_support_better_auth_1_7/migration.sql
@@ -0,0 +1,17 @@
+/*
+Warnings:
+
+- A unique constraint covering the columns `[issuer,accountId]` on the table `account` will be added. If there are existing duplicate values, this will fail.
+- Added the required column `issuer` to the `account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "account" ADD COLUMN "issuer" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "two_factor"
+ALTER COLUMN "failed_verification_count"
+DROP NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "account_issuer_accountId_uidx" ON "account" ("issuer", "accountId");
